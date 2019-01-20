2ee458576d51467a53b24b38c8888c4e77016b51
Ahmed <Ahmed_Bahajjaj@u.nus.edu>
2019-01-20 13:00:49 +0800

    Added js file

---
+++ proj_firestore.js
@@ -0,0 +1,39 @@
+const admin = require('firebase-admin');
+
+//to change
+/*
+var serviceAccount = require('C:/Users/shunf/OneDrive/Desktop/my-awesome-project-9a50a-firebase-adminsdk-l7ym1-53abc2353b.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+*/
+
+var db = admin.firestore();
+
+var docRef = db.collection('users').doc('alovelace');
+
+var setAda = docRef.set({
+  first: 'Ada',
+  last: 'Lovelace',
+  born: 1815
+});
+
+var aTuringRef = db.collection('users').doc('aturing');
+
+var setAlan = aTuringRef.set({
+  'first': 'Alan',
+  'middle': 'Mathison',
+  'last': 'Turing',
+  'born': 1912
+});
+
+db.collection('users').get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+      console.log(doc.id, '=>', doc.data());
+    });
+  })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  });
